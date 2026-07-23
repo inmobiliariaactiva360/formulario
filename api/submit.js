da0a7689c307f46c7666be92f9f1189a5d54cc91
inmobiliariaactiva360 <inmobiliariaactiva360@gmail.com>
2026-07-23 11:35:47 +0200

    Forzar despliegue con Gmail SMTP como ruta activa

--- api/submit.js
+++ api/submit.js
@@ -1,1 +1,3 @@
+// Ruta activa del formulario: Gmail SMTP mediante contraseña de aplicación.
+// Variables requeridas en Vercel: GMAIL_USER y GMAIL_APP_PASSWORD.
 export { POST } from './submit-gmail.js';
